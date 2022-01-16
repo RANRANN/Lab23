776600ce2970274fc97c015c56225850f92eca95
a.ryzhakov2011@yandex.ru <a.ryzhakov2011@yandex.ru>
2022-01-17 00:45:55 +0300

    Добавьте файлы проекта.

---
+++ Lab23/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Lab23
+{
+    internal class Program
+    {
+        static int n = Convert.ToInt32(Console.ReadLine());
+        static void Main(string[] args)
+        {
+            FactorialAsync(n);
+            Console.ReadKey();
+        }
+        static void Factorial(int n)
+        {
+            int s = 1;
+            for (int i = 1; i <= n; i++)
+            {
+                s *= i;
+                Thread.Sleep(100);
+            }
+            Console.WriteLine(s);
+        }
+        static async void FactorialAsync(int n)
+        { 
+            await Task.Run(()=>Factorial(n));
+        }
+    }
+}
